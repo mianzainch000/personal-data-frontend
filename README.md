72f6c1f67fa86d5f1b970ea4f0dd23d541f1dc13
mianzainch000 <mianzainch000@gmail.com>
2025-08-21 22:06:58 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- .env file important for deployment -->
+
+ <!-- Umer Riaz Customer ID
+1134698207
+Umer Riaz Reference Number
+19132371972191 -->
---
+++ src/app/auth/login/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginForm from './template'
+
+const LoginFormPage = () => {
+    return (
+        <LoginForm />
+    )
+}
+
+export default LoginFormPage
+
+export function generateMetadata() {
+    return { title: "Login" };
+}
+
+
