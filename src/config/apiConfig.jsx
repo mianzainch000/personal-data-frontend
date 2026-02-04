4e74cda8b7323153b56cc96b75250437d7d5651f
mianzainch000 <mianzainch000@gmail.com>
2026-02-04 11:58:59 +0500

    Initial commit

---
+++ src/config/apiConfig.jsx
@@ -0,0 +1,13 @@
+export const apiConfig = {
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  login: "login",
+  forgotPassword: "forgotPassword",
+  resetPassword: "resetPassword",
+  password: {
+    get: "get",
+    post: "create",
+    delete: "delete",
+    update: "update",
+    reorder: "reorder",
+  },
+};
